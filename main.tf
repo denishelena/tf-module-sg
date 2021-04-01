f436cf0f35eaf31e1085b28a1e130c3c12beaac1
Denis Marcelo Santa Helena <denismst@gmail.com>
2021-04-01 15:04:16 -0300

    add security group

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "new_sg" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id      = var.vpc_id
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "sg_id" {
+  value = aws_security_group.new_sg.id
+}
